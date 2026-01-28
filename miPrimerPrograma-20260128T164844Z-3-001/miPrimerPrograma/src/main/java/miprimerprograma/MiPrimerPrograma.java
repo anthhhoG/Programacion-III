f8e8f0342ca53403a5e13eedb967d2f01a1fde6f
Anthony Hetzael Suc Gomez <asucg@miumg.edu.gt>
2026-01-28 11:05:47 -0600

    Subida de programa comprimido

---
+++ miPrimerPrograma-20260128T164844Z-3-001/miPrimerPrograma/src/main/java/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerprograma;
+
+/**
+ *
+ * @author daguilae
+ */
+public class MiPrimerPrograma {
+        public static void main(String[] args) {
+            System.out.println("Hola mundo");
+            System.out.println("Anthony Hetzael Suc Gomez");
+            System.out.println("9959-24-389");
+    }
+}
